40e4a22693402d99b2e713a481f71ece48ca5d6e
Will 保哥 <doggy.huang@gmail.com>
2017-02-04 14:44:15 +0800

    12 練習 Angular 2 元件通訊：@Input()
    修改步驟：
    1. 建立 ArticleComponent 元件
    2. 搬移 HTML Template 到 Article 元件
    3. 將 *ngFor 套用在 AppComponent 裡面 ( <app-article> )
    4. 在 Article 元件設定 item 屬性，讓他可以接受屬性繫結
    5. 在 App 元件的 <app-article> 進行屬性繫結

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,18 @@
+import { Input, Component,  OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @Input()
+  item: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
