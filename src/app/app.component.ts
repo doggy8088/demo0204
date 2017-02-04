3990370962c3e06f2934de4b97f06b6c500bdebc
Will 保哥 <doggy.huang@gmail.com>
2017-02-04 14:00:03 +0800

    復原示範的 code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   keyword = 'app works!';
 
-  data: any;
-
   doSearch(str) {
     console.log(str);
     this.keyword = str;
